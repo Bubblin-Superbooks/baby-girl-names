5ea986310c652f8ead7e5e3397b8f92430f0459d
Marvin Danig <marvin@marvindanig.com>
2020-06-26 11:44:21 -0400

    Comma separated frequency.

--- countries/scripts/markupify.js
+++ countries/scripts/markupify.js
@@ -1,12 +1,10 @@
 const fs = require('fs')
 const path = require('path')
 
-fs.readFile(path.join('.', 'countries', 'usa', 'sortedBabyGirls.json'), 'utf8', async (err, data) => {
+fs.readFile(path.join('.', 'countries', 'usa', 'test.json'), 'utf8', async (err, data) => {
   if (err) throw err;
 
-	let html = JSON.parse(data).map(girl => `<div><span class="selectable">${girl.Name}</span><a data-tooltip="${girl.Frequency}" rel="nofollow noopener" href="https://duckduckgo.com/?q=${girl.Name}+name+meaning&kp=-1&kl=us-en&ia=web" class="tooltip">♕</a></div>\n`)
-
-	console.log(html)
+	let html = JSON.parse(data).map(girl => `<div><span class="small selectable">${girl.Name}</span><a data-tooltip="${parseInt(girl.Frequency).toLocaleString('en')}" rel="nofollow noopener" href="https://duckduckgo.com/?q=${girl.Name}+name+meaning&kp=-1&kl=us-en&ia=web" class="tooltip">♕</a></div>\n`)
 
 	fs.writeFile(path.join('.', 'interim', 'sanitized.html'), `<body>${html.join('')}</body>`, (err) => {
 		if (err) throw err;
