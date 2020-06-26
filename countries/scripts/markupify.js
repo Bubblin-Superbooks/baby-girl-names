bede922b0dee5e103c222bf9aff48b99bca19f17
Marvin Danig <marvin@marvindanig.com>
2020-06-26 18:53:40 -0400

    Transfer ownership.

--- countries/scripts/markupify.js
+++ countries/scripts/markupify.js
@@ -4,6 +4,7 @@ const path = require('path')
 fs.readFile(path.join('.', 'countries', 'usa', 'sortedBabyGirls.json'), 'utf8', async (err, data) => {
   if (err) throw err;
 
+
 	let html = JSON.parse(data).map(girl => `<div><span class="selectable">${girl.Name}</span><a data-tooltip="${parseInt(girl.Frequency).toLocaleString('en')}" rel="nofollow noopener" href="https://duckduckgo.com/?q=${girl.Name}+name+meaning&kp=-1&kl=us-en&ia=web" class="small tooltip">♕</a></div>\n`)
 
 	fs.writeFile(path.join('.', 'interim', 'sanitized.html'), `<body>${html.join('')}</body>`, (err) => {
